d1ca9a9db0b4cd32ae8352837d35ccc7074e3aa0
nanlan <nanlan.yj@foxmail.com>
2018-02-09 16:53:34 +0800

    verify RegForm

--- src/components/regForm.vue
+++ src/components/regForm.vue
@@ -2,50 +2,62 @@
   <div>
     <div class="login-form">
       <div class="g-form">
-        <!-- <div class="g-form-line">
-          <span class="g-form-label">用户名：</span>
-          <div class="g-form-input">
-            <input type="text" placeholder="请输入用户名" v-model="usernameModel">
+       <div class="g-form-line">
+             <span class="g-form-label">用户名</span>
+             <div class="g-form-input">
+                 <input v-validate ="'required|username'" type="text" id="email" name="myUsername">
+             </div>
+             <span v-show="errors.has('myUsername')" class="help is-danger">{{ errors.first('myUsername')}}</span>
+        </div>
+           <div class="g-form-line">
+             <span class="g-form-label">手机号码</span>
+             <div class="g-form-input">
+                 <input v-validate ="'required|phonenumber'" type="text" id="phonenumber" name="myPnumber">
+             </div>
+             <span v-show="errors.has('myPnumber')" class="help is-danger">{{ errors.first('myPnumber')}}</span>
           </div>
-          <span class="g-form-error">{{userErrors.errorText}}</span>
-        </div> -->
       
          <div class="g-form-line">
              <span class="g-form-label">邮箱</span>
              <div class="g-form-input">
-                 <input v-validate ="'required|email'" type="text" id="email" name="myEmail">
+                 <input v-validate ="'required|email'" type="text" id="email" name="myEmail" :class="{'input': true, 'is-danger': errors.has('email') }">
              </div>
-              <span v-show="errors.has('myEmail')">{{ errors.first('myEmail')}}</span>
-        </div>
+             <span v-show="errors.has('myEmail')" class="help is-danger">{{ errors.first('myEmail')}}</span>
+          </div>
            
-        <!--密码
         <div class="g-form-line">
           <span class="g-form-label">密码：</span>
           <div class="g-form-input" >
-            <input type="password"  placeholder="请输入密码" v-model="passwordModel">
+            <input type="password"  v-validate="'required|password'" id="password" name="myPassWord"  placeholder="请输入密码" >
           </div>
-          <span class="g-form-error">{{ passwordErrors.errorText}}</span>
-        </div>-->
-        <!--确认密码
+          <span v-show="errors.has('myPassWord')" class="help is-danger">{{ errors.first('myPassWord')}}</span>
+        </div>
+     
         <div class="g-form-line">
           <span class="g-form-label">确认密码：</span>
           <div class="g-form-input" >
-            <input type="password"  placeholder="请输入确认密码" v-model="repasswordModel">
+            <input type="password"  placeholder="请输入确认密码" >
           </div>
-          <span class="g-form-error">{{ repasswordErrors.errText }}</span>
+         
         </div>
-        按钮-->
-        <!-- <div class="g-form-inline">
+    
+        <div class="g-form-inline">
           <div class="g-form-btn">
-            <a class="button" @click="onLogin">注册</a>
+            <a class="button" >注册</a>
           </div>
-        </div> -->
+        </div>
+
       </div>
     </div>
   </div>
 </template>
 <script>
-
 </script>
 <style>
+input.is-danger{
+  border-color: red;
+}
+.help.is-danger{
+    color:red;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -11,13 +11,48 @@ import DetailCouPage from './pages/details/count'
 import DetailForPage from './pages/details/forecast'
 import DetailPubPage from './pages/details/publish'
 
-import VeeValidate ,{Validator} from 'vee-validate';
+import VeeValidate from 'vee-validate';
+import {Validator} from 'vee-validate';
 import zh_CN from 'vee-validate/dist/locale/zh_CN'
+import VueI18n from 'vue-i18n';
+Vue.use(VueI18n)
+const i18n = new VueI18n({
+  locale: 'zh_CN',
+})
+Vue.use(VeeValidate, {
+  i18n,
+  i18nRootKey: 'validation',
+  dictionary: {
+    zh_CN
+  }
+});
 
-const config = {
-  locale: 'zh_CN'
-};
-Vue.use(VeeValidate,config);
+const dict = {
+  messages:{
+     required:()=>'这个是必须的'
+  }
+}
+
+const validator = new Validator({});
+validator.localize('zh_CN',dict);
+Validator.extend('username', {
+  getMessage: field => '以字母或者下划线开头，可包含字母数字下划线',
+  validate: value => {
+    return  /^[a-zA-Z0-9_-]{4,10}$/.test(value)
+  }
+});
+Validator.extend('phonenumber', {
+  getMessage: field => field + '必须是11位手机号码',
+  validate: value => {
+    return value.length == 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
+  }
+});
+Validator.extend('password', {
+  getMessage: field => field + '最少6位，包括至少1字母、特殊字符、数字',
+  validate: value => {
+    return  /^.*(?=.{6,})(?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%^&*? ]).*$/.test(value)
+  }
+});
 
 
 Vue.use(VueRouter)
